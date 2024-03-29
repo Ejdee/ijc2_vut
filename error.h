88ef1b1448e99904e33375a61818d5ccb063603d
Adam Behoun <adam.behoun@seznam.cz>
2024-03-29 18:54:08 +0100

    Program that write error and exit the program

---
+++ error.h
@@ -0,0 +1,10 @@
+/* error.h
+ * Řešení IJC-DU1, příklad b)
+ * Autor: Adam Běhoun, FIT
+ * Datum: 15.3.2024
+ * login: xbehoua00
+ * Přeloženo: gcc (GCC) 10.5.0
+*/
+
+void warning(const char *fmt, ...);
+void error_exit(const char *fmt, ...);
