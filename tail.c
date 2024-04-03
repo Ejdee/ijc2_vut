1e6527056c149dc46c262258d81abc4bfb8a3cb5
Adam Behoun <adam.behoun@seznam.cz>
2024-04-03 23:39:39 +0200

    fix opt parser and printing out number of lines less than size

--- tail.c
+++ tail.c
@@ -5,14 +5,14 @@
 #include "error.h"
 
 bool cbuf_empty(cbuf *cb) {
-    return cb->count == 0;
+    return cb->count == 0; // if count of lines in the buffer is zero, then the buffer is empty
 }
 
 cbuf* cbuf_create(int n) {
-    cbuf *cb = malloc(sizeof(cbuf));
+    cbuf *cb = malloc(sizeof(cbuf)); // allocate memory for structure
 
     if (cb != NULL) {
-        cb->lines = malloc(n * sizeof(char[MAX_LINE_SIZE]));
+        cb->lines = malloc(n * sizeof(char[MAX_LINE_SIZE])); // allocate memory for each line depending on variable 'n'
         if(cb->lines == NULL) {
             free(cb);
             cb = NULL;
@@ -30,23 +30,27 @@ cbuf* cbuf_create(int n) {
 }
 
 void cbuf_put(cbuf *cb, char *line) {
+
+    // copy the line in the buffer at the current write index. to ensure that the line does not exceed the maximum length of buffer size,
+    // we copy at most MAX_LINE_SIZE - 1 characters, and we add null terminator character at the end.
     strncpy(cb->lines[cb->windex], line, MAX_LINE_SIZE-1);
     cb->lines[cb->windex][MAX_LINE_SIZE-1] = '\0';
 
+    // if the buffer is not empty and write index is equal to read index, we want to increment read index too
     if(!cbuf_empty(cb) && cb->windex == cb->rindex)
         cb->rindex = (cb->rindex + 1) % cb->size;
 
-    cb->windex = (cb->windex + 1) % cb->size;
-    cb->count ++;
+    cb->windex = (cb->windex + 1) % cb->size; // increment write index
+    cb->count ++; // increment number of lines in the buffer
 }
 
 char* cbuf_get(cbuf *cb) {
     if(cbuf_empty(cb))
         error_exit("Buffer is empty.");
 
-    char *to_return = cb->lines[cb->rindex];
-    cb->rindex = (cb->rindex + 1) % cb->size;
-    cb->count --;
+    char *to_return = cb->lines[cb->rindex]; // store line at the read index in variable
+    cb->rindex = (cb->rindex + 1) % cb->size; // increment read index
+    cb->count --; // decrement number of lines in the buffer
 
     return to_return;
 }
@@ -57,23 +61,27 @@ void cbuf_free(cbuf* cb) {
 }
 
 int main(int argc, char *argv[]) {
-    int size = 10;
-    FILE *fp = stdin;
+    int size = 10; // default number of lines we want to print out
+    FILE *fp = stdin; // default stream
 
     if(argc > 4)
         error_exit("Wrong arguments.");
 
     for(int i = 1; i < argc; i++) {
         if(strcmp("-n", argv[i]) == 0) {
-            if(i+1 <= argc) {
+            if(i+1 <= argc) { // check if i+1 argc exists
                 size = atoi(argv[i+1]);
+
+                if(size <= 0) // if -n argument is less or equal to 0
+                    error_exit("Option -n requires a number above 0.");
+
                 i++;
             } else {
-                error_exit("Wrong arguments.");
+                error_exit("Option -n requires argument.");
             }
         } else {
-            if(fp != stdin) {
-                error_exit("Wrong arguments.");
+            if(fp != stdin) { // if we are supposed to change file name, but it is not stdin.. it was already changed and we throw error
+                error_exit("You cannot type in two files.");
             }
             fp = fopen(argv[i], "r");
             if(fp == NULL) {
@@ -82,19 +90,26 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    cbuf* cb = cbuf_create(size);
+    cbuf* cb = cbuf_create(size); // create the buffer
 
+    // load the lines from file or stdin to buffer
     char line[MAX_LINE_SIZE];
     while(fgets(line, MAX_LINE_SIZE, fp) != NULL) {
         cbuf_put(cb, line);
     }
 
-    for(int i = 0; i < size; i++) {
+    // if 'size' is less than the number of lines in the buffer, print 'size' lines. otherwise print the maximum number of lines.
+    int cb_lines_count = size < cb->count ? size : cb->count;
+
+    // print out the lines
+    for(int i = 0; i < cb_lines_count; i++) {
         printf("%s", cbuf_get(cb));
     }
 
+    // free the structure
     cbuf_free(cb);
 
+    // if the file is opened, close it
     if(fp != stdin)
         fclose(fp);
 
