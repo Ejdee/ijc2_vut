b32cfc2cbf36f499bec9ddc21e118bedb4246b7f
Adam Behoun <adam.behoun@seznam.cz>
2024-04-16 12:10:07 +0200

    Finished wordcount.c

--- wordcount.c
+++ wordcount.c
@@ -7,25 +7,9 @@
 #include "htab_struct.h"
 #include "htab_item.h"
 
-#define HASH_SIZE 20
+#define HASH_SIZE 12451
 #define MAX_LENGTH_WORD 255
 
-void print_table(htab_t *table) {
-    for(int i = 0; i < table->arr_size; i++) {
-        printf("%d:", i+1);
-        if(table->ptr[i] != NULL) {
-            htab_item_t *temp = table->ptr[i];
-            while(temp != NULL) {
-                printf(" --> (%s, %d)", temp->pair.key, temp->pair.value);
-                temp = temp->next;
-            }
-            printf(" --> %p\n", (void*)temp);
-        } else {
-            printf(" --> %p\t", (void*)table->ptr[i]);
-            printf("\n");
-        }
-    }
-}
 
 void print_pair(htab_pair_t *data) {
     printf("%s\t%d\n", data->key, data->value);
@@ -43,14 +27,15 @@ int main() {
             fprintf(stderr, "A word is longer than 254 characters, cutting.\n");
             warning = true;
         }
-        printf("\n-------------------------------\n");
-        printf("%s\n", string);
-        printf("-------------------------------\n\n");
+        //printf("\n-------------------------------\n");
+        //printf("%s\n", string);
+        //printf("-------------------------------\n\n");
         htab_pair_t *new_item = htab_lookup_add(table, string);
-        printf("%s\n", new_item->key);
+        (void)new_item;
+        //printf("%s\n", new_item->key);
     }
 
-    print_table(table);
+    //print_table(table);
 
     htab_for_each(table, &print_pair);
 
