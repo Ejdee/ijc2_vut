3c20a7eba86726f887becc121c7a4a52ee95da9e
Adam Behoun <adam.behoun@seznam.cz>
2024-04-19 14:38:19 +0200

    Clean up and fix

--- tail.c
+++ tail.c
@@ -2,8 +2,10 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <ctype.h>
 
-#define MAX_LINE_SIZE 2047
+#define MAX_LINE_SIZE 2048
+#define DEFAULT_N_SIZE 10
 
 /**
  * @brief cyclic buffer structure, that stores N elements of lines, read index, write index,
@@ -17,6 +19,73 @@ typedef struct tcbuf {
     int size;
     int count;
 } cbuf;
+
+
+/**
+ * @brief check if the passed parameter is only digits
+ * 
+ * @param parameter string to check
+ * @return true if it is only digits
+ * @return false if some characters is not digit
+ */
+bool n_valid(char* parameter) {
+    for(size_t i = 0; i < strlen(parameter); i++) {
+        if(!isdigit(parameter[i])) {
+            return false;
+        }
+    } 
+    return true;
+}
+
+/**
+ * @brief handle passed arguments of the program and return the number of
+ * lines, that user wants to print out. If the -n option was specified, we
+ * calculate the new number, otherwise we return default size of 10.
+ * 
+ * @param argc number of arguments
+ * @param argv array of arguments
+ * @param fp stream
+ * @return int number of lines to print out
+ */
+int parser(int argc, char *argv[], FILE **fp) {
+    int size = -1;
+
+    if(argc > 4) {
+        fprintf(stderr, "Too many arguments");
+        exit(1);
+    }
+
+    for(int i = 1; i < argc; i++) {
+        if(strcmp("-n", argv[i]) == 0) {
+            if(i+1 < argc) { // check if i+1 argc exists
+                if(n_valid(argv[i+1])) {
+                    size = strtol(argv[i+1], NULL, 10); // convert the string to number and store it in size
+                } else {
+                    fprintf(stderr, "Invalid argument for -n option.\n");
+                    exit(1);
+                }
+
+                i++; // increment the i since we processed it 
+            } else {
+                fprintf(stderr, "Option -n requires argument.\n");
+                exit(1);
+            }
+            
+        } else {
+            if(*fp != stdin) { // if we are supposed to change file name, but it is not stdin.. it was already changed and we throw error
+                fprintf(stderr, "You cannot type in two files.\n");
+                exit(2);
+            }
+            *fp = fopen(argv[i], "r"); // open the file
+            if(*fp == NULL) { // check for success
+                fprintf(stderr, "Failed to open file %s.\n", argv[i]);
+                exit(2);
+            }
+        }
+    }
+
+    return (size > -1 ? size : DEFAULT_N_SIZE);
+}
 
 /**
  * @brief function checks if buffer of cbuf structure is empty or not
@@ -65,9 +134,6 @@ cbuf* cbuf_create(int n) {
  */
 void cbuf_put(cbuf *cb, char *line) {
 
-    if(strlen(line) > MAX_LINE_SIZE)
-        fprintf(stderr, "Line is too long, it is printed out cut on 2047 characters.\n");
-
     // copy the line in the buffer at the current write index. to ensure that the line does not exceed the maximum length of buffer size,
     // we copy at most MAX_LINE_SIZE - 1 characters, and we add null terminator character at the end.
     strncpy(cb->lines[cb->windex], line, MAX_LINE_SIZE-1);
@@ -110,50 +176,67 @@ void cbuf_free(cbuf* cb) {
     free(cb);
 }
 
-int main(int argc, char *argv[]) {
-    int size = 10; // default number of lines we want to print out
-    FILE *fp = stdin; // default stream
+/**
+ * @brief load lines in the cyclic buffer
+ * 
+ * @param dest buffer
+ * @param fp stream
+ * @param cb cyclic buffer
+ */
+void load_lines(char* dest, FILE *fp, cbuf *cb) {
+    // reading lines to dest
+    while(fgets(dest, MAX_LINE_SIZE, fp) != NULL) {
 
-    if(argc > 4) {
-        fprintf(stderr, "Wrong arguments.\n");
-        return 1;
-    }
+        bool newline_read = false;
+        // checking, if the loaded line contains newline character
+        for(size_t i = 0; i < strlen(dest); i++) {
+            if(dest[i] == '\n') {
+                newline_read = true;
+                break;
+            } 
+        }
 
-    for(int i = 1; i < argc; i++) {
-        if(strcmp("-n", argv[i]) == 0) {
-            if(i+1 <= argc) { // check if i+1 argc exists
-                size = atoi(argv[i+1]);
+        bool warning = false;
+        if(!newline_read) {
+            // if the new line character is not in the string, we will insert it before the EOF
+            //dest[MAX_LINE_SIZE-2] = '\n';
 
-                if(size <= 0){ // if -n argument is less or equal to 0
-                    fprintf(stderr, "Option -n requires a number above 0.\n");
-                    return 1;
+            if(!warning) {
+                fprintf(stderr, "Some lines are too long, it is being cut to 2047 characters.\n");
+                warning = true;
+            }
+
+            // throw away all the characters that exceeded the length limit
+            int c = 0;
+            while((c = getc(fp)) != '\n') {
+                if(c == EOF) {
+                    break;
                 }
-
-                i++;
-            } else {
-                fprintf(stderr, "Option -n requires argument.\n");
-                return 1;
-            }
-        } else {
-            if(fp != stdin) { // if we are supposed to change file name, but it is not stdin.. it was already changed and we throw error
-                fprintf(stderr, "You cannot type in two files.\n");
-                return 1;
-            }
-            fp = fopen(argv[i], "r");
-            if(fp == NULL) {
-                fprintf(stderr, "Failed to open file %s.\n", argv[i]);
-                return 1;
             }
         }
+
+        cbuf_put(cb, dest);
+    }
+}
+
+
+int main(int argc, char *argv[]) {
+    FILE *fp = stdin; // default stream
+
+    int size = parser(argc, argv, &fp); // wee need to pass pointer to fp
+
+    if(size == 0) { // if number of lines to print out is 0, end the program with return code 0
+        if(fp!=stdin){
+            fclose(fp);
+        }
+        return 0;
     }
 
     cbuf* cb = cbuf_create(size); // create the buffer
 
     // load the lines from file or stdin to buffer
     char line[MAX_LINE_SIZE];
-    while(fgets(line, MAX_LINE_SIZE, fp) != NULL) {
-        cbuf_put(cb, line);
-    }
+    load_lines(line, fp, cb);
 
     // if 'size' is less than the number of lines in the buffer, print 'size' lines. otherwise print the maximum number of lines.
     int cb_lines_count = size < cb->count ? size : cb->count;
